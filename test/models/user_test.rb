11d8a27f87ac185210ba0449eb044d9eaedf6163
pipinosuke <ys.pipinosuke@gmail.com>
2016-09-07 22:54:21 +0900

    init6.3

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
-  test setup do
-    @user=User.new(name:"ベジータ",email:"ベジータ@gmail.com")
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   test "valid?" do
@@ -24,6 +21,34 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
 
   end
+
+  test "name should not be too long" do
+    @user.name="a"*51
+    assert_not @user.valid?
+
+  end
+
+  test "email not too long" do
+    @user.email="a"*244+"@example.com"
+    assert_not @user.valid?
+
+  end
+
+  test "email valid?" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                           first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        @user.email = valid_address
+        assert @user.valid?, "#{valid_address.inspect} should be valid"
+      end
+    end
+
+  test "email addresses should be unique" do
+  duplicate_user = @user.dup
+  duplicate_user.email = @user.email.upcase
+  @user.save
+  assert_not duplicate_user.valid?
+end
 
 
 
