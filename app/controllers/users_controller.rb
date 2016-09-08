5d70f44b263f451b1cf2499c9076fbe9a7ede776
pipinosuke <ys.pipinosuke@gmail.com>
2016-09-08 22:41:13 +0900

    ?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
+      flash[:success]="汚い花火だ"
+      redirect_to @user
+      
+
 
     else
       render 'new'
