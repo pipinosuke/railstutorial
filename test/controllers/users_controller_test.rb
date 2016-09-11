d98b17abf529bd16e8aee08ba4ea419d1ccf57e1
pipinosuke <ys.pipinosuke@gmail.com>
2016-09-12 05:18:33 +0900

    finish chap9

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -45,4 +45,19 @@ test "should redirect update when logged in as wrong user" do
   assert_redirected_to root_url
 end
 
+test "should redirect destroy when not logged in" do
+  assert_no_difference 'User.count' do
+    delete :destroy, id: @user
+  end
+  assert_redirected_to login_url
+end
+
+test "should redirect destroy when logged in as a non-admin" do
+  log_in_as(@other_user)
+  assert_no_difference 'User.count' do
+    delete :destroy, id: @user
+  end
+  assert_redirected_to root_url
+end
+
 end
