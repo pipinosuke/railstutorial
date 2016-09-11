328a78a8d1cc8a59b582258c75c1abed4600bb92
pipinosuke <ys.pipinosuke@gmail.com>
2016-09-11 18:07:04 +0900

    init9.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
     @user=User.new
   end
@@ -56,4 +58,11 @@ class UsersController < ApplicationController
       redirect_to login_url
     end
   end
+
+  def correct_user
+       @user = User.find(params[:id])
+       redirect_to(root_url) unless current_user?(@user)
+  end
+
+
 end
