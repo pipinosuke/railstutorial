a0977cb1bdf6ba4664fbb46a62bd413908219a9d
pipinosuke <ys.pipinosuke@gmail.com>
2016-09-11 18:02:42 +0900

    init 9.2.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def new
     @user=User.new
   end
@@ -24,7 +25,7 @@ class UsersController < ApplicationController
 
   def edit
   @user = User.find(params[:id])
-end
+  end
 
 
   def index
@@ -39,7 +40,7 @@ end
 
     else
     render 'edit'
-  end
+    end
   end
 
   private
@@ -47,5 +48,12 @@ end
   def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
 end
