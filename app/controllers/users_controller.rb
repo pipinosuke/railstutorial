0a1906ef7dc5ed8aa9e08d266dda75ac53cdf44d
pipinosuke <ys.pipinosuke@gmail.com>
2016-09-12 03:51:16 +0900

    9.3.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,11 +29,11 @@ class UsersController < ApplicationController
   @user = User.find(params[:id])
   end
 
-
   def index
-    @users = User.all
-
+      @users = User.paginate(page: params[:page])
   end
+
+
 
   def update
     @user=User.find(params[:id])
