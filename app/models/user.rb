6300ece5f5575f35a2a06ddeb8ff4629b934f304
pipinosuke <ys.pipinosuke@gmail.com>
2016-09-22 15:37:22 +0900

    confflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,7 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
   attr_accessor :remember_token, :activation_token, :reset_token
-=======
-<<<<<<< HEAD
-  attr_accessor :remember_token, :activation_token
->>>>>>> cd9206e7cc137fe7a5b3d118af71babe22eb7405
   before_save :downcase_email
   before_create :create_activation_digest
-=======
-
-before_create :create_activation_digest
-  attr_accessor :remember_token, :activation_token
-  before_save   :downcase_email
->>>>>>> a10bfc58d22038db71883d0c55841ed599741361
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
