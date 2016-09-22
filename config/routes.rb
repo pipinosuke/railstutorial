0cae6bbadc30a1af23fedb81414889f852d6c42d
pipinosuke <ys.pipinosuke@gmail.com>
2016-09-22 15:09:24 +0900

    init 10.2.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
@@ -11,4 +15,5 @@ post   'login'   => 'sessions#create'
 delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
